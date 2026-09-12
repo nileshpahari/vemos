a68414737f6d810a3acdc7fcb62f75a447a94c3b
Nilesh Kumar Pahari <nileshpahari@protonmail.com>
2026-09-12 15:00:27 +0530

    add bootloader crate

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,18 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static MSSG: &[u8] = b"HELLO WORLD";
+
 #[unsafe(no_mangle)]
 extern "C" fn _start()-> ! {
-    // entrypoint
+    let vga_buf = 0xb8000 as *mut u8;
+
+    for (i, &byte) in MSSG.iter().enumerate() {
+        unsafe {
+            *vga_buf.offset(i as isize * 2) = byte;
+            *vga_buf.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop{}
 }
